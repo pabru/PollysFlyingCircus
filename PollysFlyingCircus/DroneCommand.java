654de67a9c0a2350a607af7bbf7f436538b31183
pabru <1310028@my.brunel.ac.uk>
2015-05-19 20:40:00 +0100

    A DroneCommand class to get an enum of the type of command to send, combine it with the current sequence number and any user parameters and turn the combination into an AT* command String

---
+++ PollysFlyingCircus/DroneCommand.java
@@ -0,0 +1,55 @@
+package PollysFlyingCircus;
+
+
+public class DroneCommand {
+
+	public static enum PCMD{
+		PITCH_GO_FORWARD(",1,", ",0,0,0");
+
+		private String prefix;
+		private String postfix;
+
+		private PCMD(String prefix, String  postfix){
+			this.prefix = prefix;
+			this.postfix = postfix;
+		}
+
+		public String getPrefix(){
+			return prefix;
+		}
+		public String getPostfix(){
+			return postfix;
+		}
+	}
+
+	public static enum REF{
+		TAKEOFF(290718208),
+		LAND(290717696);
+
+		private int value;
+
+		private REF(int value){
+			this.value = value;
+		}
+
+		public int getValue(){
+			return value;
+		}
+	}
+
+	private final static String pcmdATPrefix = "AT*PCMD=";
+	private final static String refATPrefix = "AT*REF=";
+	
+	public static String toString(int sequenceNumber, PCMD pcmdCommand, float parameter){
+		return pcmdATPrefix + sequenceNumber + "," + 
+				pcmdCommand.prefix + 
+				DroneFloatToIntEncoder.encode(parameter) + 
+				pcmdCommand.postfix;
+	}
+
+	public String toString(int sequenceNumber, REF refCommand){
+		return refATPrefix + sequenceNumber + "," + 
+				refCommand.value;
+	}
+
+}
