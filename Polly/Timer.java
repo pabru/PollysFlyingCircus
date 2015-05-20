b7f695f3bd186d741553656db34e935a37cfe9d5
Piotr Andruszkow (Brunel) <1310028@my.brunel.ac.uk>
2015-05-20 10:45:15 +0100

    Created a general-purpose Timer class

---
+++ Polly/Timer.java
@@ -0,0 +1,24 @@
+package Polly;
+
+public class Timer {
+	double startSystemTimeNanoseconds = 0;
+	double stopSystemTimeNanoseconds = 0;
+	
+	public void startTimer(){
+		startSystemTimeNanoseconds = System.nanoTime();
+	}
+	public double stopTimer(){
+		stopSystemTimeNanoseconds = System.nanoTime();
+		return getTimeElapsedNanoseconds();
+	}
+	public double getTimeElapsedNanoseconds() {
+		return stopSystemTimeNanoseconds - startSystemTimeNanoseconds;
+	}
+	public double getTimeElapsedMilliseconds(){
+		return getTimeElapsedNanoseconds()/1000;
+	}
+	public void resetTimer(){
+		startSystemTimeNanoseconds = 0;
+		stopSystemTimeNanoseconds = 0;
+	}
+}
