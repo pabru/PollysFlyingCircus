331b5e7a584e4dd4dc00e533ad102e6d8f7d085a
Piotr Andruszkow (Brunel) <1310028@my.brunel.ac.uk>
2015-05-28 21:29:09 +0100

    Add the Progress Check Day drone dance with spinning

--- polly/Command/Test.java
+++ polly/Command/Test.java
@@ -1,36 +1,59 @@
 package polly.Command;
 
+import polly.imagecapture.QRReaderTest;
+
 
 public class Test {
 	
 	public static void main(String args[]) throws Exception
 	{
 		Drone.initialise();
-		Drone.trimFlat();
 		Thread.sleep(2500);
 		Drone.takeOff();
 		
 		Thread.sleep(4000);
 		
-		routine2();
+		naviQR();
 		
 		Drone.land();
 		Drone.disconnect();
 	}
 	
-	private static void routine2() throws Exception{
+	private static void naviQR() throws Exception{
 		
 		long t= System.currentTimeMillis();
-		long end = t+700;
+		long end = t+300;
 		while(System.currentTimeMillis() < end) {
 			Drone.goUp(1f);
 		}
 		
+		
+		
 		long t1= System.currentTimeMillis();
-		long end1 = t1+4000;
-		while(System.currentTimeMillis() < end1) {
-			
-			Drone.goForward(1f);
+		long end1 = t1+2000;
+//		String qrDecoded = null;
+		while(System.currentTimeMillis() < end1 ) {
+//			qrDecoded = QRReaderTest.getQRBelow();
+//			System.err.println("exiting qr recog");
+			Drone.goForward(0.5f);
+		}
+		
+		long t2= System.currentTimeMillis();
+		long end2 = t2+3000;
+//		String qrDecoded = null;
+		while(System.currentTimeMillis() < end2 ) {
+//			qrDecoded = QRReaderTest.getQRBelow();
+//			System.err.println("exiting qr recog");
+			Drone.goForward(-0.5f);
+		}
+		
+		long t3= System.currentTimeMillis();
+		long end3 = t3+3142;
+//		String qrDecoded = null;
+		while(System.currentTimeMillis() < end3 ) {
+//			qrDecoded = QRReaderTest.getQRBelow();
+//			System.err.println("exiting qr recog");
+			Drone.turnClockWise(-1f);;
 		}
 		
 		
